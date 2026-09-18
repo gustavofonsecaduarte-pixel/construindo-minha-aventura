ef42bfad029ef4014c45450cd3c4dcec2a78f739
gustavofonsecaduarte-pixel <gustavo.fonseca.duarte@escola.pr.gov.br>
2026-09-18 10:10:12 -0300

    1020

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const avaça = document.querySelectorAll
+const avanca document.querySelectorAll('.btn-proximo');
+
+
+
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+
+const proximoPasso passo-' + this.getAttribute('data-proximo');
+
+atual.classList.remove('ativo');
+
+
+   })
+})
